64bccca9fa0a65eacf4e080fdba109ca8b0b5895
zanatooo <m8elzanaty@gmail.com>
2022-05-05 19:30:25 -0700

    adding files

---
+++ index.js
@@ -0,0 +1,87 @@
+const inquirer = require ('inquirer')
+const fs = require('fs')
+function start () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name:"title",
+            message:"project title"
+        },
+        {
+            type: "input",
+            name: "description",
+            message:"project description"
+        },
+        {
+            type: "input",
+            name:"installation",
+            message:"project installation"
+        },
+        {
+            type: "input",
+            name:"usage",
+            message:"project usage requirments"
+        },
+        {
+            type: "list",
+            name:"lisence",
+            message:"project lisence",
+            choices: ['MIT','ISC','APPACHE2.0','GPL','NONE']
+        },
+        {
+            type: "input",
+            name:"contribution",
+            message:"project contributor"
+        },
+        {
+            type: "input",
+            name:"test",
+            message:"project testing requirements"
+        },
+        {
+            type: "input",
+            name:"email",
+            message:"project emialing"
+        },
+        {
+            type: "input",
+            name:"github",
+            message:"github profile"
+        }
+   
+    ]).then (function(response){
+    let readmeContent=`
+    # Title:${response.title}
+    ## Description: ${response.description}
+    ### Table of Content:
+    * [license](license)
+    * [instatllation](#installation)
+    * [usage](#usage)
+    * [contribution](#contribution)
+    * [testing](#testing)
+    * [github](#github)
+    * [email](#email)
+
+    #### License:
+    ![GitHub license](https://img.shields.io/badge/license-${response.license}-blue.svg)
+
+    #### Installation:
+    ${response.installation}
+
+    #### Usage:
+    ${response.usage}
+
+    #### Conribution:
+    ${response.contribution}
+
+    #### Github:
+    [github profile](https://github.com/${response.github})
+
+    `
+    fs.writeFileSync('OUTPUT.md',readmeContent,function(error){
+        if(error)throw error
+    })
+console.log('Good work today')
+    })
+}
+start()
